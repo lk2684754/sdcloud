7d0eb062e55b7e6b8b75cbbda99ee5dc2da54de5
lk2684754 <91539980+lk2684754@users.noreply.github.com>
2022-04-05 21:40:46 +0800

    Add files via upload

---
+++ dist/p__menu__index.0712c155.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[17],{uRXc:function(e,c,n){"use strict";n.r(c);var t=n("q1tI"),s=n("9kvl"),u=n("nKUr"),a=e=>(Object(t["useEffect"])((()=>{s["b"].replace("/menu/home")}),[]),Object(u["jsx"])(u["Fragment"],{}));c["default"]=a}}]);
